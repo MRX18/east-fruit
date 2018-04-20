3bbc2e138dd16dbb934ecf98349f5182756aa2f8
MRX18 <roma.borisenko18@gmail.com>
2018-04-20 20:42:23 +0300

    Установка zizako

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         SleepingOwl\Admin\Providers\SleepingOwlServiceProvider::class,
-
+        Zizaco\Entrust\EntrustServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -226,6 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
 
     ],
 
