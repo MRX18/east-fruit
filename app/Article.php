1a391c9c82a58ffa2154083a9a3b760d85dca6cd
MRX18 <roma.borisenko18@gmail.com>
2018-04-27 16:07:29 +0300

    Сделал правки по времени

--- app/Article.php
+++ app/Article.php
@@ -9,29 +9,35 @@ class Article extends Model
 {
 
     public function sitebar($count) {
+        date_default_timezone_set('Europe/Kiev');
         $date = Carbon::now()->toDateTimeString();
     	return $this->where('datetime','<=',$date)->orderByDesc('datetime')->limit($count)->get();
     }
 
     public function articleInIndexPage($namePole, $data, $count) {
+        date_default_timezone_set('Europe/Kiev');
         $date = Carbon::now()->toDateTimeString();
     	return $this->where('datetime','<=',$date)->where($namePole, $data)->orderByDesc('datetime')->limit($count)->get();
     }
 
     public function lineIndex($namePole, $data) {
+        date_default_timezone_set('Europe/Kiev');
         $date = Carbon::now()->toDateTimeString();
     	return $this->where('datetime','<=',$date)->where($namePole,$data)->orderByDesc('datetime')->first();
     }
     /********Atricle page********/
     public function article($id) {
+        date_default_timezone_set('Europe/Kiev');
     	return $this->where('id', $id)->first();
     }
     /********Catigor page********/
     public function articleCatigor($idCatigor, $count) {
+        date_default_timezone_set('Europe/Kiev');
         $date = Carbon::now()->toDateTimeString();
     	return $this->where('datetime','<=',$date)->where('id_catigories', $idCatigor)->orderByDesc('datetime')->paginate($count);
     }
     public function allArticles($count) {
+        date_default_timezone_set('Europe/Kiev');
         $date = Carbon::now()->toDateTimeString();
     	return $this->where('datetime','<=',$date)->orderByDesc('datetime')->paginate($count);
     }
